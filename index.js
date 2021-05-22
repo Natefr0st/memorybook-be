b97c4489cf418d277586c794d62332ea32fedf72
Kevin <kevin_zino@yahoo.com>
2021-05-23 00:59:55 +0300

    Setup server and connect to DB

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+	'mongodb+srv://natefr0st:CqlM1SX1HXE3Y7mJ@memorybook.zjd7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.port || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+	)
+	.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
