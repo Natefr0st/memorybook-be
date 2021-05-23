95b632960aa2fa6f758fd02e96f8fa1f7197daed
Kevin <kevin_zino@yahoo.com>
2021-05-23 19:41:06 +0300

    Added Route && Controller for the update functionality

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
 	'mongodb+srv://natefr0st:CqlM1SX1HXE3Y7mJ@memorybook.zjd7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
